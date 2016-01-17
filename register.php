ef8d45ce528053f5ef7f1cb91fdacf2a7cfcc9fe
Nicholas Lago <lagond@mcmaster.ca>
2016-01-17 09:54:10 -0800

    thunderdome

--- register.php
+++ register.php
@@ -6,30 +6,30 @@ $password="Cantsee123"; // Mysql password
 $db_name="marmot"; // Database name 
 $tbl_name="[dbo].[UserAccount]"; // Table name 
 $connectionOptions = array("Database"=>$db_name, "Uid"=>$username, "PWD"=>$password)
-sqlsrv_connect("$host", conectionOptions)or die("cannot connect"); 
+sqlsrv_connect("host", conectionOptions)or die("cannot connect"); 
 
 
-// $myusername=$_POST['myusername']; 
-// $myfirstname=$_POST['myfirstname']; 
-// $mylastname=$_POST['mylastname']; 
-// $mytwitch=$_POST['mytwitch']; 
-// $mylinkedin=$_POST['mylinkedin']; 
-// $mytwitter=$_POST['mytwitter']; 
+$myusername=$_POST['myusername']; 
+$myfirstname=$_POST['myfirstname']; 
+$mylastname=$_POST['mylastname']; 
+$mytwitch=$_POST['mytwitch']; 
+$mylinkedin=$_POST['mylinkedin']; 
+$mytwitter=$_POST['mytwitter']; 
 
-// $myusername = stripslashes($myusername);
-// $myfirstname=stripslashes($myfirstname);
-// $mylastname=stripslashes($mylastname); 
-// $mytwitch=stripslashes($mytwitch); 
-// $mylinkedin=stripslashes($mylinkedin); 
-// $mytwitter=stripslashes($mytwitter); 
-// $myusername = mysql_real_escape_string($myusername);
-// $mylastname=mysql_real_escape_string['mylastname']; 
-// $mytwitch=mysql_real_escape_string['mytwitch']; 
-// $mylinkedin=mysql_real_escape_string['mylinkedin']; 
-// $mytwitter=mysql_real_escape_string['mytwitter']; 
-// $sql="INSERT INTO [dbo].[UserAccount] VALUES ($mytwitter,$mylinkedin,null,$mytwitch,$myfirstname,$mylastname";
+$myusername = stripslashes($myusername);
+$myfirstname=stripslashes($myfirstname);
+$mylastname=stripslashes($mylastname); 
+$mytwitch=stripslashes($mytwitch); 
+$mylinkedin=stripslashes($mylinkedin); 
+$mytwitter=stripslashes($mytwitter); 
+$myusername = mysql_real_escape_string($myusername);
+$mylastname=mysql_real_escape_string['mylastname'];
+$mytwitch=mysql_real_escape_string['mytwitch']; 
+$mylinkedin=mysql_real_escape_string['mylinkedin']; 
+$mytwitter=mysql_real_escape_string['mytwitter']; 
+$sql="INSERT INTO [dbo].[UserAccount] VALUES ($mytwitter,$mylinkedin,null,$mytwitch,$myfirstname,$mylastname";
 
-// header("location:www.welcometothethunderdo.me");
+header("location:www.welcometothethunderdo.me");
 
 /*
 // Mysql_num_row is counting table row
